81beeb88c27c1bc3473dc039f41b7211a099a6ee
obieglok <obieglok@tcd.ie>
2020-05-25 16:23:20 +0100

    Created tetrominoes
    Created the tetrominoes and all all their positions

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
-document.addEventListener('DOMContentLoader', () =>){
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelectorAll('#score') // # shows id
+  const StartBtn = document.querySelectorAll('#start-button')
+  const width = 10
+  // the tetrominoes
+  const lTetrmonino = [
+    // starts at position 1, then the next one is drawn at 11, 21 and 2
+    // l shaped
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+  // z shaped
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+  // tshaped
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+  // square shaped
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+  const theTetrominoes = [lTetrmonino, zTetromino, tTetromino, oTetromino, iTetromino]
 
-
-  
-}
+  let currentPosition = 4
+  let current = theTetrominoes[0][0]
+  // draw the first rotation
+  function draw () {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+  draw()
+})
