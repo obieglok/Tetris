feb1fe03879809f60c8458f71efb6b846dcbf31c
obieglok <obieglok@tcd.ie>
2020-05-26 15:10:00 +0100

    Added move left,down and right functionality

--- app.js
+++ app.js
@@ -43,12 +43,85 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetrmonino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
-  let current = theTetrominoes[0][0]
-  // draw the first rotation
+  let currentRotation = 0
+  // randomly select a tetromino and its first rotation
+  let random = Math.floor(Math.random() * theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
+  // draw the tetromino
   function draw () {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
     })
   }
-  draw()
+
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+  //make the tetrominoes move down every second
+  timerId = setInterval(moveDown, 1000)
+
+  //assign functions to keycode
+//  console.log(10)
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    }
+    else if(e.keyCode === 38)
+    {
+      //rotate()
+    }
+    else if(e.keyCode === 39)
+    {
+      moveRight()
+    }
+    else if(e.keyCode === 40)
+    {
+      moveDown()
+    }
+  }
+
+  document.addEventListener("keyup", control)
+
+  function moveDown () {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  // freeze function
+  function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      //start a new tetrominoe
+      random=Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+     }
+  }
+  // stop tetromino from going offscreen left
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    //if it isnt at the most left position, we can move left by one square
+    if(!isAtLeftEdge) currentPosition -=1
+    // if there is already something in the way do not move there
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition +=1
+    }
+    draw()
+  }
+  function moveRight(){
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+    if(!isAtRightEdge) currentPosition +=1
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+      currentPosition -=1
+    }
+    draw()
+  }
+
 })
